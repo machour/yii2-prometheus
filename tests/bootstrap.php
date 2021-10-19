75275885c1cf9759a863704e37fba405df6b492b
Mehdi Achour <machour@gmail.com>
2021-10-19 08:39:29 +0100

    Use yii's phpunit patches instead

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,22 +8,6 @@ const YII_DEBUG = true;
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-if (!function_exists('each')) {
-    function each(array &$array) {
-        $value = current($array);
-        $key = key($array);
-
-        if (is_null($key)) {
-            return false;
-        }
-
-        // Move pointer.
-        next($array);
-
-        return array(1 => $value, 'value' => $value, 0 => $key, 'key' => $key);
-    }
-}
-
 $vendorRoot = __DIR__ . '/../vendor';
 
 require_once($vendorRoot . '/autoload.php');
